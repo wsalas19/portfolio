c05afe07766e1216c62e467577d87ee7eb6dddd8
William Salas <willbullet52@gmail.com>
2026-04-12 11:10:04 -0500

    new cf config

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,17 +3,22 @@ const config = {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy" ,
+      tagCache: "dummy" ,
+      queue: "dummy" ,
     },
-  },
+	},
+  edgeExternals: ["node:crypto"],
   middleware: {
     external: true,
     override: {
       wrapper: "cloudflare-edge",
       converter: "edge",
       proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 };
