8d3cbf3c47e318c06ace3163f11b00f0233eec03
William Salas <willbullet52@gmail.com>
2026-06-21 20:41:04 -0500

    Fix terminal DPR to 1

--- src/components/FaultyTerminal.tsx
+++ src/components/FaultyTerminal.tsx
@@ -261,7 +261,7 @@ export default function FaultyTerminal({
 	tint = "#ffffff",
 	mouseReact = true,
 	mouseStrength = 0.2,
-	dpr = Math.min(window?.devicePixelRatio || 1, 2),
+	dpr = 1,
 	pageLoadAnimation = true,
 	brightness = 1,
 	className,
